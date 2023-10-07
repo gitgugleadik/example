20231b10930733e8a2a0db7adeaccc9191b39dfb
gugleadik <gugleadik@yandex.ru>
2023-10-07 23:39:18 +0500

    Добавляю

--- example1.py
+++ example1.py
@@ -1,3 +1,4 @@
+#https://habr.com/ru/companies/vdsina/articles/563432/
 # Этот пример может печатать любую строку n раз без использования циклов Python.
 
 n=5
@@ -9,3 +10,19 @@ a=3
 b=4
 a, b = b, a
 print(a, b) # a= 4, b =3
+
+
+# Поиск подстроки
+programmers = ["I'm an expert Python Programmer",
+               "I'm an expert Javascript Programmer",
+               "I'm a professional Python Programmer"
+               "I'm a beginner C++ Programmer"
+]
+#method 1
+for p in programmers:
+    if p.find("Python"):
+        print(p)
+#method 2
+for p in programmers:
+    if "Python" in p:
+        print(p)
