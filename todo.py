b2440fd8c6214602b3e49586292432e7a1c41eb9
gugleadik <gugleadik@yandex.ru>
2023-10-23 00:08:15 +0500

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,3 +1,12 @@
 Найти текст какой-то речи 
 Цель провести анализ речи
 Искать в инете как делать анализ речи, тут может быть признаки (словарный запас, слова паразиты, и другое)
+https://muzei-moskvy.ru/analiz-reci-ponyatie-i-metody/#:~:text=Анализ%20речи%20–%20это%20процесс,медицину%20и%20даже%20судебные%20процессы
+Анализ устной речи
+     https://dzen.ru/a/Wz0emrRjlgCqyDhF
+Что такое психолингвистический анализ речи?
+https://blog.studylie.ru/psiholingvisticheskij-analiz-rechi/
+https://spravochnick.ru/psihologiya/psihologicheskiy_analiz/psihologicheskiy_analiz_rechi/
+https://anticoach.org/speech-analysis
+
+https://vc.ru/begovatovd/722188-5-rossiyskih-servisov-dlya-rechevoy-analitiki-svodnaya-tablica-resheniy-s-cenami
